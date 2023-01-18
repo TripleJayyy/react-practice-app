874143acfd6bf548b9d99aff8f86373d23f9d9e4
TripleJah <orangehedge@protonmail.com>
2023-01-18 14:59:55 -0600

    reminder functionality

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -3,31 +3,49 @@ import Tasks from "./components/Tasks";
 import { useState } from "react";
 
 const App = () => {
-  const [tasks, setTasks] = useState(
-    [{
-        id: 1,
-        text: "Doctor",
-        day: "Feb 5",
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: "School",
-        day: "Feb 6",
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: "Food",
-        day: "Feb 7",
-        reminder: false,
-      },
-    ])
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor",
+      day: "Feb 5",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "School",
+      day: "Feb 6",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food",
+      day: "Feb 7",
+      reminder: false,
+    },
+  ]);
+
+  // delete item function
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Reminder Function
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks, add some!"
+      )}
     </div>
   );
 };
